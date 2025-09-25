e0e7dd7f90a7b4b4a014c12cb4a64a6e568438ca
Riteshkumarsingh1 <164739146+Riteshkumarsingh1@users.noreply.github.com>
2025-09-25 19:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (20.17%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,11 +1,25 @@
 class Solution {
     public String toLowerCase(String s) {
-        char c[] = s.toCharArray();
-        for(int i =0; i< c.length; i++){
-            if(c[i]>=65 && c[i]<=90){
-                c[i] =(char) (c[i]+32);
+        //return s.toLowerCase();                     // direct method 
+
+
+        // char c[] = s.toCharArray();
+        // for(int i =0; i< c.length; i++){
+        //     if(c[i]>=65 && c[i]<=90){                 // method 2
+        //         c[i] =(char) (c[i]+32);
+        //     }
+        // }
+        // return String.valueOf(c);   
+
+
+        StringBuilder sb = new StringBuilder();
+        for(char c: s.toCharArray()){
+            if(c>=65 && c<= 90){   //c=b                  // using String Builders
+                c = (char)(c+32);  // c=A, c= a
             }
+             sb.append(c);
         }
-        return String.valueOf(c);
+       return sb.toString();
     }
+
 }
