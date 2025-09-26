83bd6fbc1cad68e7b648b2cb786d384faa8bd76d
Riteshkumarsingh1 <164739146+Riteshkumarsingh1@users.noreply.github.com>
2025-09-27 01:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (47.41%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+      // linear search method
+
+          // using while loop
+    //   int i=0;
+    //   while(arr[i]<arr[i+1]){
+    //     i++;
+    //   }   
+    //   return i;
+
+      // using for loop
+    //   for(int i =0; i< arr.length;i++){
+    //     if (arr[i]>arr[i+1]){
+    //         return i;
+    //     }
+    //   }
+    //   return -1;
+
+
+
+    //bimary search method:::::
+    int low =0, high=arr.length-1;
+    while(low<high){
+        int mid = low+(high-low)/2;
+        if(arr[mid]<arr[mid+1]){
+            low=mid+1;
+        }else{
+            high=mid;
+        }
+    }
+    return low;
+    }
+}
