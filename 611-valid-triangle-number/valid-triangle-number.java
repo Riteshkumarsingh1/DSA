5a8cae3e1d8a7fcd6d6a3ba250cab1cbaaeb6d01
Riteshkumarsingh1 <164739146+Riteshkumarsingh1@users.noreply.github.com>
2025-09-28 01:49:09 +0530

    Time: 23 ms (82.93%) | Memory: 43.3 MB (62.11%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.java
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -1,21 +1,44 @@
+// import java.util.Arrays;
+
+// class Solution {
+//     public int triangleNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length, ans = 0;
+//         for (int k = n - 1; k >= 2; --k) {          // k is the largest side
+//             int i = 0, j = k - 1;
+//             while (i < j) {
+//                 if (nums[i] + nums[j] > nums[k]) {
+//                     ans += j - i;
+//                     j--;
+//                 } else {
+//                     i++;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
 import java.util.Arrays;
 
 class Solution {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length, ans = 0;
-        for (int k = n - 1; k >= 2; --k) {          // k is the largest side
+        int n = nums.length, count = 0;
+        for (int k = n - 1; k >= 2; k--) {
             int i = 0, j = k - 1;
             while (i < j) {
                 if (nums[i] + nums[j] > nums[k]) {
-                    ans += j - i;
+                    count += j - i;
                     j--;
                 } else {
                     i++;
                 }
             }
         }
-        return ans;
+        return count;
     }
 }
 
