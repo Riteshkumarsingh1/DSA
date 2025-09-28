01dfa40af00ec46f09784ebba871f2da5863a7ef
Riteshkumarsingh1 <164739146+Riteshkumarsingh1@users.noreply.github.com>
2025-09-28 14:31:30 +0530

    Time: 1 ms (89.49%) | Memory: 42 MB (97.42%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -14,13 +14,24 @@ class Solution {
 
 
 
-        //taking base as 26::::;
+        //taking base as 26::::
+        /* example :
+            
+           AZA
+        place value of all wiil be as:
+        from backward ---  initially sum =0
+        A=1*(26)^0       sum=0+1=1
+        Z=26*(26)^1      sum=1+(26*26)=677
+        A=1*(26)^2       sum = 677+676 = 1353
+        
+        finally answer, for AZA is === 1353
+         */
 
-        int sum=0,p=0;
-        int n= columnTitle.length();
-        for (int i=n-1;i>=0;i--){
-            int temp = columnTitle.charAt(i)-64;
-            sum= sum+(int)Math.pow(26,p++)*temp;
+        int sum=0,p=0;   // p here is position of of charater
+        int n= columnTitle.length();    // traversing the length of columnTitle from backward right to left
+        for (int i=n-1;i>=0;i--){     // backtracing element from right to left
+            int temp = columnTitle.charAt(i)-64;  //finding the location of char/ element from 1 to 26
+            sum= sum+(int)Math.pow(26,p++)*temp;  //returning the place value of each charater
 
         }
         return sum;
