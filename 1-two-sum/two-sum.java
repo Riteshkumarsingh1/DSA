39b6f6207353d1e838361d483df2f51d34461770
Riteshkumarsingh1 <164739146+Riteshkumarsingh1@users.noreply.github.com>
2025-09-29 00:03:18 +0530

    Time: 2 ms (98.91%) | Memory: 45.2 MB (31.83%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,13 +2,27 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
      //brut force approach
 
-     for (int i =0; i< nums.length - 1 ; i++){
-        for (int j=i+1; j< nums.length; j++){
-            if (nums[i] + nums[j] == target){
-                return new int[]{i,j};
-            }
+    //  for (int i =0; i< nums.length - 1 ; i++){
+    //     for (int j=i+1; j< nums.length; j++){
+    //         if (nums[i] + nums[j] == target){
+    //             return new int[]{i,j};
+    //         }
+    //     }
+    //  }   
+    //  return new int []{};
+
+
+
+    //using HashMap:::::::
+
+    HashMap<Integer,Integer>map = new HashMap<Integer,Integer>();
+    for(int i =0; i<nums.length; i++){   // iterate over arr
+        if(map.containsKey(nums[i])){    // check for key like here 9-2=7 , so 7 is a key here
+            return new int []    {map.get(nums[i]),i};    //create new arr  map={key,value}
+        }else{
+            map.put(target-nums[i],i);  // else put map={key,i};
         }
-     }   
-     return new int []{};
+    }
+    return new int [ ]{};
     }
 }
