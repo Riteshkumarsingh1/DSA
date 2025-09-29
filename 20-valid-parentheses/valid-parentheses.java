b8bd93e9284b8730efddd221d9cae3aaeafdd077
Riteshkumarsingh1 <164739146+Riteshkumarsingh1@users.noreply.github.com>
2025-09-30 01:50:34 +0530

    Time: 3 ms (50.76%) | Memory: 42.3 MB (12.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,20 +1,53 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         char[]arr = s. toCharArray();
+//         Stack <Character> stack = new Stack<>();
+//         for(int i=0; i<arr.length; i++){
+//             char a= arr[i];
+//             if(stack.isEmpty()){
+//                 stack.push(a);
+//             }else{
+//                 char top = stack.peek();
+//                 if(a-top == 1|| a- top ==2){
+//                     stack.pop();
+//                 }else{
+//                     stack.push(a);
+//                 }
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
+
+
+
+
+
+//other bruet force approach:::
 class Solution {
     public boolean isValid(String s) {
-        char[]arr = s. toCharArray();
-        Stack <Character> stack = new Stack<>();
-        for(int i=0; i<arr.length; i++){
-            char a= arr[i];
-            if(stack.isEmpty()){
-                stack.push(a);
-            }else{
-                char top = stack.peek();
-                if(a-top == 1|| a- top ==2){
-                    stack.pop();
-                }else{
-                    stack.push(a);
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            // Push every opening bracket to the stack
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                // If stack is empty, no opening bracket for current closing
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                // Check for correct matching
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
                 }
             }
         }
+        // If stack is empty, all brackets are matched correctly
         return stack.isEmpty();
     }
 }
