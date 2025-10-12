4345d1de8839d3e5775a1967d69664f380f9d121
RITESH KUMAR SINGH <164739146+Riteshkumarsingh1@users.noreply.github.com>
2025-10-12 21:53:01 +0530

    Time: 4 ms (99.00%) | Memory: 57.3 MB (13.47%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -30,7 +30,7 @@
 
 
 
-//other solution
+//other solution BUBBLE SORT
 class Solution {
     public int[] sortArray(int[] nums) {
         if(nums == null || nums.length <= 1) return nums;
@@ -60,4 +60,44 @@ class Solution {
         }
         return nums;
     }
+}
+
+
+
+
+
+
+///ANOTHER SOLUTION USING SELECTION SORT
+public class selectionSort {
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 2, 1 };
+        selection(arr);
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
 }
