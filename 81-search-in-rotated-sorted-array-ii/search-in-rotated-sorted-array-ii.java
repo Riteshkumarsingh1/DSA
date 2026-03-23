08279c2423d83bfe01e1924f0a76a308e9e3c410
RITESH KUMAR SINGH <164739146+Riteshkumarsingh1@users.noreply.github.com>
2026-03-24 01:35:26 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (76.25%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+       
+        while (left <= right) {
+            
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+       
+            if (nums[left] < nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[left] > nums[mid]) {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                left++;
+            }
+        }
+
+     return false;
+    }
+}
+
+public class _81_Search_in_Rotated_Sorted_Array_II {
+    
+    public static void main(String[] args) {
+        int[] nums = new int[] { 2, 5, 6, 0, 0, 1, 2 };
+        int target = 0;
+
+        boolean result = new Solution().search(nums, target);
+
+        System.out.println("The target is" + (result ? " " : " not ") + "in rotated sorted array.");
+    }
+}
