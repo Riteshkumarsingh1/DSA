2c3292b4c85e996ceefa79ca38e32ff57f82b607
RITESH KUMAR SINGH <164739146+Riteshkumarsingh1@users.noreply.github.com>
2026-04-03 03:32:55 +0530

    Time: 170 ms (37.36%) | Memory: 278 MB (19.78%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -49,3 +49,41 @@ class Solution {
         return dp[r][c][neutLeft] = result;
     }
 }
+
+
+
+
+//backtracking method     but TLE
+// class Solution {
+//     public int maximumAmount(int[][] coins) {
+//         return backtrack(coins, 0, 0, 2);
+//     }
+    
+//     int backtrack(int[][] coins, int r, int c, int neutLeft) {
+//         int m = coins.length, n = coins[0].length;
+        
+//         // Out of bounds
+//         if (r >= m || c >= n) return Integer.MIN_VALUE / 2;
+        
+//         // Reached end
+//         if (r == m-1 && c == n-1) {
+//             return neutLeft > 0 ? Math.max(0, coins[r][c]) : coins[r][c];
+//         }
+        
+//         int curr = coins[r][c];
+//         int result = curr + Math.max(
+//             backtrack(coins, r+1, c, neutLeft),   // Down
+//             backtrack(coins, r, c+1, neutLeft)    // Right
+//         );
+        
+//         // Neutralize here if possible
+//         if (curr < 0 && neutLeft > 0) {
+//             result = Math.max(result, Math.max(
+//                 backtrack(coins, r+1, c, neutLeft-1),
+//                 backtrack(coins, r, c+1, neutLeft-1)
+//             ));
+//         }
+        
+//         return result;
+//     }
+// }
